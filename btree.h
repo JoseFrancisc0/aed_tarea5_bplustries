421a1e94da7ee26effd9672ea187a2faebaced4e
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-02-05 10:27:11 -0500

    Template for btree

--- btree.h
+++ btree.h
@@ -1,6 +1,21 @@
 #ifndef B_TREE_H
 #define B_TREE_H
 
-class BTree{};
+template<typename T>
+class BTree{
+    private:
+        struct Node{};
+
+        Node* root;
+    public:
+        BTree(): root(nullptr){};
+
+        void insert(T data);
+        void erase(T key);
+        bool find(T key);
+        void clear();
+
+        ~BTree();
+};
 
 #endif //B_TREE_H
