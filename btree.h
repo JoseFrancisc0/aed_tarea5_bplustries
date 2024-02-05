6013ee0efa8c427eb7c5c171a0973730b13c1e9d
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-02-05 10:33:05 -0500

    Basic structure of BTree

--- btree.h
+++ btree.h
@@ -1,21 +1,47 @@
 #ifndef B_TREE_H
 #define B_TREE_H
 
+#include <vector>
+using namespace std;
+
 template<typename T>
 class BTree{
     private:
-        struct Node{};
+        struct Node{
+            vector<T> keys;
+            vector<Node*> children;
+            Node* father;
+            bool leaf;
+
+            Node(): father(this), leaf(true){};
+        };
 
         Node* root;
+
+        void insert(T data, Node* current);
+        void erase(T key, Node* current);
+        bool find(T key, Node* current);
+
     public:
         BTree(): root(nullptr){};
 
-        void insert(T data);
-        void erase(T key);
-        bool find(T key);
+        void insert(T data){
+            insert(data, root);
+        }
+
+        void erase(T key){
+            erase(key, root);
+        }
+
+        bool find(T key){
+            find(key, root);
+        }
+
         void clear();
 
-        ~BTree();
+        ~BTree(){
+            clear();
+        }
 };
 
 #endif //B_TREE_H
