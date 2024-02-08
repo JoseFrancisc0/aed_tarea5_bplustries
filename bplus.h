cb02c5de96591c65701cc04ebb7b02ae010951d1
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-02-08 16:20:46 -0500

    Basic template for B+Tree

--- bplus.h
+++ bplus.h
@@ -1,6 +1,22 @@
 #ifndef B_PLUS_TREE
 #define B_PLUS_TREE
 
-class BPlus_Tree{};
+class BPlus_Tree{
+    private:
+        struct Node{
+            // TODO: MEMBERS
+
+            Node(){}; // TODO: Constructor
+        };
+        Node* root;
+        int order; //Minimum degree (defines the range for number of keys)
+
+    public:
+        BPlus_Tree(int M): root(nullptr), order(M){};
+
+        void insert(int k);
+        void remove(int k);
+        bool search(int k);
+};
 
 #endif //B_PLUS_TREE
