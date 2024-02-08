3284193f33023a640c3d3e28bead19ddca1e6376
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-02-08 18:05:40 -0500

    Progress of insert()

--- bplus.h
+++ bplus.h
@@ -8,10 +8,10 @@ class BPlus_Tree{
             int n;  // #keys in node
             int order;
             Node** C; // children of node
-            Node* nextLeaf = nullptr; // link leafs
+            Node* nextLeaf; // link leafs
             bool leaf;
 
-            Node(bool l, int t): leaf(l), order(t){
+            Node(bool l, int t): leaf(l), order(t), nextLeaf(nullptr){
                 keys = new int[2*order - 1];
                 C = new Node *[2*order];
                 n = 0;
@@ -20,6 +20,57 @@ class BPlus_Tree{
             ~Node(){
                 delete[] keys;
                 delete[] C;
+            }
+
+            void split_child(int i, Node* y){
+                // TODO: Checkear si nodo a splittear es hoja
+                // Si es hoja, guardar la key mediana en la hoja izquierda
+                // hojaIzquierda->nextLeaf = hojaDerecha
+                // ESTE ES CODE DEL BTREE
+                Node* z = new Node(order, y->leaf);
+                z->n = order - 1;
+                for (int j = 0; j < order - 1; j++)
+                    z->keys[j] = y->keys[j + order];
+
+                if (y->leaf == false){
+                    for (int j = 0; j < order; j++)
+                        z->C[j] = y->C[j + order];
+                }
+
+                y->n = order - 1;
+                for (int j = n; j >= i + 1; j--)
+                    C[j + 1] = C[j];
+                C[i + 1] = z;
+
+                for (int j = n - 1; j >= i; j--)
+                    keys[j + 1] = keys[j];
+
+                keys[i] = y->keys[order - 1];
+                n = n + 1;                    
+            }
+
+            void insert_non_full(int k){
+                int i = n - 1;
+
+                if (leaf == true){
+                    while (i >= 0 && keys[i] > k){
+                        keys[i + 1] = keys[i];
+                        i--;
+                    }
+                    keys[i + 1] = k;
+                    n = n + 1;
+                }
+                else{
+                    while (i >= 0 && keys[i] > k)
+                        i--;
+
+                    if (C[i + 1]->n == 2*order - 1){
+                        split_child(i + 1, C[i + 1]);
+                        if (keys[i + 1] < k)
+                            i++;
+                    }
+                    C[i + 1]->insert_non_full(k);
+                }  
             }
 
             Node* search(int k){
@@ -43,7 +94,29 @@ class BPlus_Tree{
     public:
         BPlus_Tree(int M): root(nullptr), order(M){};
 
-        void insert(int k);
+        void insert(int k){
+            if(root == nullptr){
+                root = new Node(true, order);
+                root->keys[0] = k;
+                root->n = 1;
+            }
+            else{
+                if(root->n == 2*order - 1){
+                    Node* nodo = new Node(order, false);
+                    nodo->C[0] = root;
+                    nodo->split_child(0, root);
+
+                    int i = 0;
+                    if(nodo->keys[0] < k)
+                        i++;    
+                    nodo->C[i]->insert_non_full(k);
+
+                    root = nodo;
+                }
+                else
+                    root->insert_non_full(k);
+            }
+        }
 
         void remove(int k);
 
