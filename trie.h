4467b00f40f540fc1bcb7647398d88ae61b061fd
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-02-08 13:53:48 -0500

    Basic Trie template

--- trie.h
+++ trie.h
@@ -1,6 +1,32 @@
 #ifndef TRIE_H
 #define TRIE_H
 
-class Trie{};
+#include <list>
+#include <string>
+using namespace std;
+
+class Trie{
+    private:
+        struct Node{
+            list<pair<char, Node*>> children;
+            bool endWord;
+
+            Node(): endWord(false){}; // Implement constructor
+        };
+        
+        Node* root;
+
+    public:
+        Trie(): root(nullptr){};
+
+        void insert(string s);
+        void erase(string s);
+        bool search(string s);
+        void clear();
+
+        ~Trie(){
+            clear();
+        }
+};
 
 #endif //TRIE_H
