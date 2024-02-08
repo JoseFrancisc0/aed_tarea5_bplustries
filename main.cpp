6807066a2b8b198ba5265f63f8860bf9a9f1c768
JoseFrancisc0 <jose.wong@utec.edu.pe>
2024-02-08 15:20:10 -0500

    Trie fully implemented

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
 #include <iostream>
+#include "trie.h"
+using namespace std;
+
+void test_trie(){
+    Trie trie;
+    trie.insert("chi");
+    trie.insert("chino");
+    trie.insert("chip");
+    trie.insert("chipi");
+
+    cout << boolalpha << trie.search("chip") << endl;
+    cout << boolalpha << trie.search("chipi") << endl;
+    trie.erase("chipi");
+    cout << boolalpha << trie.search("chip") << endl;
+    cout << boolalpha << trie.search("chipi") << endl;
+}
 
 int main(int, char**){
     std::cout << "Hello, from TAREA_SEMANA_5!\n";
